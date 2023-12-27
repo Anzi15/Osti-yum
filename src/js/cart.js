0aadc3ba1bce014b4b7d2730f4702b996a2ff994
Lemme.code <djam4343@gmail.com>
2023-12-27 15:01:22 +0500

    empty cart

---
+++ src/js/cart.js
@@ -0,0 +1,14 @@
+const cart_items = localStorage.getItem("cart_items");
+
+const cartStatus = function(){
+    if(!cart_items){
+        "m"
+    }else{
+        loadCartItems()
+    }
+}
+const loadCartItems = function(cartItems){
+    
+}
+
+cartStatus()
