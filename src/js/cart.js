92c1a12d7f65a267bef7305d26f060ae3d9d9183
Lemme.code <djam4343@gmail.com>
2023-12-31 21:30:07 +0500

    fixed multiple id bug

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,19 +2,29 @@ const cart_items = localStorage.getItem("cart_items");
 const empty_cart_sec = document.getElementById('empty_cart_sec');
 const cart_product = document.getElementById('product-cart');
 
+async function fecther(){
+    await fetch('../src/json/products.json')
+    .then(response => response.json())
+    .then(data => filterCartItems(cart_items,data));
+}
+
 const cartEmpty = function(cartItems){
   return cartItems == null ?   true : false;
 }
 
-const loadCartItems = function(cartItems){
-    
+const filterCartItems = function(cartItems,data){
+    const cart_items_Arr = JSON.parse(cartItems)
+    console.log(``,data,cart_items_Arr)
+    cart_items_Arr.forEach(item => {
+        
+    });
 }
 
 function cartEmptyUpdater(cart_section){
     if(cartEmpty(cart_items)){
         cart_product.classList.add("none")
         cart_section.classList.remove("none")
-        cart_section.innerHTML =  `            <div class="cart-illustration">
+        cart_section.innerHTML =  `<div class="cart-illustration">
         <img src="../src/assets/Groupcart.svg" alt="" class="illustration" draggable="false" >
     </div>
 
@@ -25,6 +35,8 @@ function cartEmptyUpdater(cart_section){
             <a href="../catalog/">Continue shopping</a>
         </div>
     </div>`
+    }else{
+        fecther()
     }
 }
 
--- src/js/individual_product.js
+++ src/js/individual_product.js
@@ -1,6 +1,6 @@
-const quantityInput = document.getElementById('quantity-inp');
-const qttyPlusBtn = document.getElementById('qtty-pls-btn');
-const qttyMinusBtn = document.getElementById('qtty-mns-btn');
+const quantityInput = document.querySelectorAll('#quantity-inp');
+const qttyPlusBtn = document.querySelectorAll('#qtty-pls-btn');
+const qttyMinusBtn = document.querySelectorAll('#qtty-mns-btn');
 const product_imgs_btn = document.querySelectorAll(".product_imgs")
 
 product_imgs_btn.forEach(elem =>{
@@ -11,37 +11,48 @@ product_imgs_btn.forEach(elem =>{
 
 function img_modal(elem){
     if(elem){
-        console.log(``,elem.classList)
         elem.classList.toggle("full-screen")
     }
-    // elem.classlist.add("full-screen")
 }
 
 const plusOne = function(elem){
-    const elemValue = parseInt(elem.value);
-    elem.value = elemValue + 1;
+    elem.forEach(element=>{
+        const elemValue = parseInt(element.value);
+        element.value = elemValue + 1;
+    })
 }
 const minusOne = function(elem){
-    const elemValue = parseInt(elem.value);
-    if(elemValue > 1){
-        elem.value = elemValue - 1;
-    }
+    elem.forEach(element=>{
+        const elemValue = parseInt(element.value);
+        if(elemValue > 1){
+            element.value = elemValue - 1;
+        }
+    })
 }
 const quantityUpdater = function(elem,action){
     action(elem)
 }
 
 //not letting quantity go below 1
-quantityInput.addEventListener("change",()=>{
-    if(quantityInput.value < 1){
-        quantityInput.value = 1
-    }
-})
+
+quantityInput.forEach(element => {
+    element.addEventListener("change",()=>{
+        if(element.value < 1){
+            element.value = 1
+        }
+    })
+});
+
 
 //updating the quantity respectively
-qttyMinusBtn.addEventListener("click",()=>{
-    quantityUpdater(quantityInput,minusOne)
+qttyMinusBtn.forEach(element =>{
+    element.addEventListener("click",()=>{
+        quantityUpdater(quantityInput,minusOne)
+}) 
 })
-qttyPlusBtn.addEventListener("click",()=>{
-    quantityUpdater(quantityInput,plusOne)
-})
+
+qttyPlusBtn.forEach(element => {
+    element.addEventListener("click",()=>{
+        quantityUpdater(quantityInput,plusOne)
+    })
+}); 
